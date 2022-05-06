9778d5f0b4ee4b4f7a54e178c0bbe1c99c3ee89c
ees-salmi <56280703+ees-salmi@users.noreply.github.com>
2022-05-06 00:43:21 +0000

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'ESSALMI ELMUSTAPHA';
+    : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'COURS DE SOUTIEN EN INFORMATIQUE';
+    : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'INSCRIPTION EN BAS .';
+    : 'All rights reserved.';
 
   return {
     name,
